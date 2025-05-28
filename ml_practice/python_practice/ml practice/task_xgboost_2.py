23f62e63ce1109f416d62de04661d4487a867b73
suvagiyasahil112 <suvagiyasahil360@gmail.com>
2025-05-28 21:42:06 +0530

    Update task_xgboost_2.py

--- ml_practice/python_practice/ml practice/task_xgboost_2.py
+++ ml_practice/python_practice/ml practice/task_xgboost_2.py
@@ -24,6 +24,7 @@ oh_df = pd.DataFrame(onehotencoder,columns=ohe.get_feature_names_out(categorical
 df = pd.concat([df,oh_df],axis=1)
 # print(df.head())
 df = df.drop(categorical_columns,axis=1)
+df = df.dropna()
 
 # print(df.sample(9))
 print(df.columns)
@@ -32,8 +33,8 @@ df = df.drop("Personality_Introvert",axis=1)
 x = df.drop(["Personality_Extrovert"],axis=1)
 # print(x.head(6))
 y = df[["Personality_Extrovert"]]
-print(x.head(6))
-
+# print(x.head(6))
+print(df.info())
 x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=42)
 
 
@@ -43,9 +44,9 @@ dtest = xgb.DMatrix(x_test,label=y_test)
 
 params = {
     'objective': 'multi:softprob', 
-    'num_class': 3,  
+    'num_class': 2,  
     'max_depth': 3, 
-    'eta': 0.1, 
+    'eta': 0.01, 
     'subsample': 0.8,  
     'colsample_bytree': 0.8,
     'lambda': 1.0, 
@@ -59,13 +60,23 @@ params = {
 
 cv_results= xgb.cv(dtrain=dtrain,params=params, nfold=5,num_boost_round =100,
                    early_stopping_rounds=10,metrics='mlogloss',as_pandas=True,seed=42)
-best_numm_boost_round=  cv_results['test-mloglos-mean'].idxmin()
+best_numm_boost_round=  cv_results['test-mlogloss-mean'].idxmin()
 model = xgb.train(params,dtrain ,num_boost_round=best_numm_boost_round)
 
 y_pred_prob = model.predict(dtest)
 y_pred = y_pred_prob.argmax(axis=1)
 acc = accuracy_score(y_test,y_pred)
 print(f"accuracy score:{acc*100:.2f}%")
+print(y.value_counts())
+from sklearn.metrics import confusion_matrix
+print(confusion_matrix(y_test, y_pred))
+
+import pandas as pd
+
+# Assuming your target is in a Series or DataFrame column named 'target'
+num_classes = y.nunique()
+print(f"Number of unique classes: {num_classes}")
+
 
 
 model.save_model('xgboost_model2_personality.json')     
