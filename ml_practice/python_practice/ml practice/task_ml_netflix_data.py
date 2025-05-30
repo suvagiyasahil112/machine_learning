82f2ee05af78ea18b8b87137b902efdcc70655c4
suvagiyasahil112 <suvagiyasahil360@gmail.com>
2025-05-30 11:52:36 +0530

    Create task_ml_netflix_data.py

---
+++ ml_practice/python_practice/ml practice/task_ml_netflix_data.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy  as np 
+import math
+import time 
+import matplotlib.pyplot as plt 
+
+
+
+df = pd.read_csv("ml_practice/python_practice/ml practice/netflix_titles.csv")
+print(df.head(23))
