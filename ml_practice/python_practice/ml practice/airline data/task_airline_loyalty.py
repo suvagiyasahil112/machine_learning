ac1b26a24a2ff60dd35bd93358948f84b9ea544b
suvagiyasahil112 <suvagiyasahil360@gmail.com>
2025-06-03 10:34:53 +0530

    3-6-25 10:35

---
+++ ml_practice/python_practice/ml practice/airline data/task_airline_loyalty.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import numpy as np 
+import math 
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+dictionary = pd.read_csv("Airline Loyalty Data Dictionary.csv")
+calander = pd.read_csv("Calendar.csv")
+loyalty = pd.read_csv("Customer Loyalty History.csv")
+activity = pd.read_csv("Customer Flight Activity.csv")
+print(dictionary.head(19))
+print(dictionary.info())
+print(dictionary.describe())
+
+
+# print(calander.info())
+
+print(activity.info())
+
+# print(loyalty.info())
+
+customers = activity.groupby('Loyalty Number',as_index=False).sum()
+print(''' 
+sfdaj
+      cawfs
+      wf
+      awfc
+      aw''' )
+print(customers.head(20))
+print(activity.head(20))
+
+final =pd.merge(loyalty,customers,on='Loyalty Number',how='inner')
+# print(final.info())
+
+print(final.isnull().sum())
+print(final.nunique())
+
+
+final_encoded = pd.get_dummies(final,columns=['Gender','Loyalty Card'])
+
+print(final_encoded)
+# final_encoded.to_csv("123.csv")
+
+# Create a correlation matrix
+num = final_encoded.select_dtypes(include=['int64', 'float64', 'boolean']).columns
+
+correlation_matrix = final_encoded[num].corr()
+
+print(final.columns)
+final['Cancelled'] = final['Cancellation Year'].notna().astype(int)
+
+print(final.head(12))
+final = final.drop(["Cancellation Month","Enrollment Month"],axis=1)
+
+plt.figure(figsize=(10,8))
+sns.histplot(x=final['Loyalty Card'],y=final["CLV"])
+
+plt.show()
+
--- ml_practice/python_practice/ml practice/task_ml_netflix_data.py
+++ ml_practice/python_practice/ml practice/task_ml_netflix_data.py
@@ -5,6 +5,13 @@ import time
 import matplotlib.pyplot as plt 
 
 
-
-df = pd.read_csv("ml_practice/python_practice/ml practice/netflix_titles.csv")
+df = pd.read_csv("netflix_titles.csv")
 print(df.head(23))
+print(df.info())
+print(df.isnull().all())
+
+
+# categoraical = df.select_dtypes(include='object').columns.to_list()
+# print(categoraical)
+
+
